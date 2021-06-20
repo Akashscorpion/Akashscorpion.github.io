6c3dc250f4570810f2ae77c79cfb243fa493a75c
Akashscorpion <gayaakash@gmail.com>
2021-06-21 01:52:04 +0530

    first commit

---
+++ src/pages/Head.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import "../styles/Head.css"
+
+
+function Home() {
+    return (
+        <div>
+            <header class="header-area header-sticky">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <nav class="main-nav">
+    
+                        <a href="#" class="logo11">
+                            <span className="logo1">F-kart</span>
+                        </a>
+                     
+                        <a class='menu-trigger'>
+                            <span>Menu</span>
+                        </a>
+                   
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+        </div>
+    )
+}
+
+export default Home
